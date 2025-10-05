0f14733fc4dfd48e5617316919cebe09ef7671d3
PhaniHarika <phaniharikasoma24@gmail.com>
2025-10-05 21:56:32 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+# Simple CNN for MNIST
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.ReLU()(x)
+        x = self.conv2(x)
+        x = nn.ReLU()(x)
+        x = nn.MaxPool2d(2)(x)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = nn.ReLU()(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        return nn.LogSoftmax(dim=1)(x)
+
+# Training setup
+transform = transforms.Compose([transforms.ToTensor()])
+train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = Net().to(device)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Train for 1 epoch (quick training)
+model.train()
+for epoch in range(1):
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = nn.NLLLoss()(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            pri
